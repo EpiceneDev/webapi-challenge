5d6f2022e3bd1f905dbc12aa1711cda7ae036db0
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-11-01 13:34:42 -0700

    MVP met

--- middleware/actionRouter.js
+++ middleware/actionRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const validateActionId = require("./validateActionId.js");
 const router = express.Router();
+const validateActionId = require("./validateActionId.js");
+const validateAction = require("./validateAction.js");
 const Actions = require("../actions/actionModel.js");
 
 router.get("/", (req, res) => {
@@ -21,6 +22,47 @@ router.get("/:id", validateActionId, (req, res) => {
     })
     .catch(err => {
       res.status(500).json({ message: "Error to retriving user." });
+    });
+});
+
+router.post("/", validateAction, (req, res) => {
+  Actions.insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error adding the Action."
+      });
+    });
+});
+
+router.delete("/:id", validateActionId, (req, res) => {
+  const id = req.params.id;
+  Actions.remove(id)
+    .then(countDeleted => {
+      if (countDeleted) {
+        res
+          .status(200)
+          .json({ message: `Action with ${id} has been deleted`, id: id });
+      } else {
+        res.status(404).json({ errorMessage: "No action found." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "internal Server Error" });
+    });
+});
+
+router.put("/:id", validateAction, validateActionId, (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  Actions.update(id, changes)
+    .then(action => {
+      res.status(201).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error Updating Action" });
     });
 });
 
