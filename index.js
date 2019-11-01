6af6693fd5b6e47046624221f6e8ab57fb9e35f4
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-11-01 09:23:04 -0700

    server up

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+const express = require('express');
+
+// 1
+const projectRouter = require('./middleware/projectRouter.js');
+
+const server = express();
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Posts API</h>
+    <p>Welcome to the Lambda Posts API</p>
+  `);
+});
+
+// 2
+server.use('/data/helpers', projectRouter);
+
+// server.listen(4000, () => {
+//   console.log('\n*** Server Running on http://localhost:4000 ***\n');
+// });
+const port = process.env.PORT || 8000;
+server.listen(port, () => console.log(`Listening on port ${port}...`));
